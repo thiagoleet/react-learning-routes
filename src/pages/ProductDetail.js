e934369675f06ed01da9dc0193044c022896c377
Thiago Leite <thiago.leite@agileengine.com>
2023-03-13 17:39:06 -0300

    - Using relative routes

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,20 +1,14 @@
-import { useParams, useNavigate } from "react-router-dom";
-import { products } from "../data/products";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
   const params = useParams();
-  const navigate = useNavigate();
-
-  function navigateHandler() {
-    navigate("/products");
-  }
 
   return (
     <>
       <h1>Product Details!</h1>
       <p>{params.productId}</p>
       <p>
-        <button onClick={navigateHandler}>Back to products</button>
+        <Link to=".." relative="path">Back</Link>
       </p>
     </>
   );
