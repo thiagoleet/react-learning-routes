e5317c3ff58ea7f790ebf285866727003fcec540
Thiago Leite <thiago.leite@agileengine.com>
2023-03-13 16:53:15 -0300

    - Navigating progammatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>Home Page</h1>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
